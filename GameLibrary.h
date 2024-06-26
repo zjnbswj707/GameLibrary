af2384f5006b8cdb431bf41b9dbadaa9484780c8
zjnbswj707 <119771250+zjnbswj707@users.noreply.github.com>
2024-06-26 19:04:27 +0800

    Create GameLibrary.h

---
+++ GameLibrary.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "APIDraw.h"
+#include "Control.h"
+//#include "Database.h"
+#include "GameFrame.h"
+#include "Object.h"
+#include "Picture.h"
+#include "Rand.h"
+#include "Render.h"
+#include "RGB.h"
+#include "Threading.h"
+#include "Vector2.h"
